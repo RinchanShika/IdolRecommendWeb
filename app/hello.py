6703a6b81a5b8a49c3ee2f72ffc92a6a07be7b9b
Kyasu <yasu.goesto.2d@gmail.com>
2019-07-04 23:57:45 +0900

    aiueo

--- app/hello.py
+++ app/hello.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template #追加
+import app.service as service
 
 app = Flask(__name__)
+input_evaluations = []
 
 
 @app.route('/')
 def top_page():
-    return render_template('index.html', title='Recommend Your OSHIMEN') #変更
+    return render_template('hello.html', title='Recommend Your OSHIMEN')
 
 
 @app.route('/like')
-def test1():
-    name = "test"
-    return render_template('hello.html', title='flask test', name=name) #変更
+def like():
+    if len(input_evaluations) == 5:
+        service.add_evaluation(input_evaluations, id)
+    return render_template('hello.html', title='flask test')
+
+
+@app.route('/start')
+def start():
+    id = service.add_evaluationRow()
+    return render_template('watchIdols.html', title='Watch Cute and Beautiful Girls')
 
 
 # おまじない
--- app/service.py
+++ app/service.py
@@ -1,2 +1,12 @@
 import app.dataAccessor as dao
 
+
+# start→evaluationにユーザ評価行を登録する
+def add_evaluationRow():
+    return dao.add_evaluationRow()
+
+
+def add_evaluation(result, id):
+    dao.add_evaluation(result, id)
+
+# 相関を求める
