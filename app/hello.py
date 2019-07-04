e3d4f2fdbd09d4fd1c67277ee6edb26681083145
Kyasu <yasu.goesto.2d@gmail.com>
2019-07-04 22:27:57 +0900

    はじめてのこみっと

---
+++ app/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template #追加
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def top_page():
+    return render_template('index.html', title='Recommend Your OSHIMEN') #変更
+
+
+@app.route('/test1')
+def test1():
+    name = "test"
+    return render_template('index.html', title='flask test', name=name) #変更
+
+
+# おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
