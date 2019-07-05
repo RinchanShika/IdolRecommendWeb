543dc8d8728ab8944fa84f20da0091d3617cd439
Kyasu <yasu.goesto.2d@gmail.com>
2019-07-05 19:55:59 +0900

    ugoiteyo

--- app/dataAccessor.py
+++ app/dataAccessor.py
@@ -17,18 +17,15 @@ def create_table():
         c.execute(create_member_table)
         create_evaluation_table = '''create table if not exists evaluation(id integer primary key autoincrement)'''
         c.execute(create_evaluation_table)
-
         # 値設定
         insert_member_sql = '''insert into member(name,group_name) values(?,?)'''
         # add_column_to_evaluation = ''' alter table evaluation add column ? [ int]'''
-
         for group in MemberList.groupList:
             for i in range(len(group)):
                 if i == 0:
                     continue
                 c.execute(insert_member_sql, (group[i], group[0]))
                 c.execute(' alter table evaluation add column ' + str(group[i]) + ' [ int]')
-
         conn.commit()
 
 
@@ -46,12 +43,10 @@ def add_member(name, group_name):
     dbname = 'IdolRecommendWebDB'
     with closing(sqlite3.connect(dbname)) as conn:
         c = conn.cursor()
-
         # 値設定
         insert_member_sql = '''insert into member(name,group_name) values(?,?)'''
         c.execute(insert_member_sql, (name, group_name))
         c.execute(' alter table evaluation add column ' + str(name) + ' [ int]')
-
         conn.commit()
 
 
@@ -63,7 +58,6 @@ def add_evaluationRow():
         c = conn.cursor()
         c.execute('insert into evaluation default values')
         id = c.lastrowid
-        print(id)
         conn.commit()
     return id
 
@@ -75,11 +69,7 @@ def add_evaluation(result, id):
     with closing(sqlite3.connect(dbname)) as conn:
         c = conn.cursor()
         for key in result.keys():
-            print(key)
-            print(result[key])
-            print(id)
             update_sql = "update evaluation set " + str(key) + " = '" + str(result[key]) + "' where id = " + str(id)
-            print(update_sql)
             c.execute(update_sql)
         conn.commit()
 
--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template #追加
+from flask import Flask, render_template, jsonify, session
 import app.service as service
+import glob
+import os
 
 app = Flask(__name__)
+app.secret_key = 'auhgushfuwe'
+
 input_evaluations = []
 
 
@@ -17,10 +21,28 @@ def like():
     return render_template('hello.html', title='flask test')
 
 
-@app.route('/start')
+@app.route('/watchGirls')
 def start():
     id = service.add_evaluationRow()
+    session['id'] = id
     return render_template('watchIdols.html', title='Watch Cute and Beautiful Girls')
+
+
+@app.route('/getFirstList')
+def get_first_list():
+    # 初期の名前と画像配列を与える(とりあえず来栖の写真1枚)
+    name = [1]
+    image = ['/static/img/img1/rinchan2019070100123135.jpg']
+
+    data = [
+        {"name": name},
+        {"image": image}
+    ]
+
+    return jsonify({
+        'status': 'OK',
+        'data': data
+    })
 
 
 # おまじない
